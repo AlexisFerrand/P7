3133ea3c0ecf7146f0275be1d77586cce233fda2
Alexis Ferrand <evaldo.ferrand@outlook.fr>
2022-09-14 19:20:58 +0200

    final back

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,6 +1,11 @@
 const PostModel = require('../models/post.model');
 const UserModel = require('../models/user.model');
 const ObjectId = require('mongoose').Types.ObjectId;
+const { uploadErrors } = require('../utils/error.utils');
+const fs = require('fs');
+const { promisify } = require('util');
+const pipeline = promisify(require('stream').pipeline);
+let stream = require('stream');
 
 module.exports.readPost = (req, res) => {
   PostModel.find((err, docs) => {
@@ -10,9 +15,40 @@ module.exports.readPost = (req, res) => {
 };
 
 module.exports.createPost = async (req, res) => {
+  let fileName;
+
+  if (req.file !== null) {
+    try {
+      // Vérification du format de l'image
+      if (
+        req.file.mimetype !== 'image/jpg' &&
+        req.file.mimetype !== 'image/png' &&
+        req.file.mimetype !== 'image/jpeg'
+      )
+        throw Error('invalid file');
+      if (req.file.size > 500000)
+        //en Ko | quand on throw une erreur on arrête le try catch et on passe directement au catch
+        throw Error('max size');
+    } catch (err) {
+      const errors = uploadErrors(err);
+      return res.status(201).json({ errors });
+    }
+
+    fileName = req.body.posterId + Date.now() + '.jpg';
+    let bufferStream = new stream.PassThrough();
+    await pipeline(
+      // file va créer dans ce chemin la un fichier entièrement
+      bufferStream.end(req.file.buffer),
+      fs.createWriteStream(
+        `${__dirname}/../client/public/uploads/posts/${fileName}`
+      )
+    );
+  }
+
   const newPost = new PostModel({
     posterId: req.body.posterId,
     message: req.body.message,
+    picture: req.file !== null ? './uploads/posts/' + fileName : '',
     video: req.body.video,
     likers: [],
     comments: [],
