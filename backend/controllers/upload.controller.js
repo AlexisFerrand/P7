c1b2069adb3ddf8759f541902716c2845d8332d3
Alexis Ferrand <evaldo.ferrand@outlook.fr>
2022-09-14 18:55:35 +0200

    upload image size + format check

--- backend/controllers/upload.controller.js
+++ backend/controllers/upload.controller.js
@@ -10,10 +10,11 @@ module.exports.uploadProfil = async (req, res) => {
   try {
     // Vérification du format de l'image
     if (
-      req.file.detectedMimeType !== 'image/jpg' &&
-      req.file.detectedMimeType !== 'image/png' &&
-      req.file.detectedMimeType !== 'image/jpeg'
+      req.file.mimetype !== 'image/jpg' &&
+      req.file.mimetype !== 'image/png' &&
+      req.file.mimetype !== 'image/jpeg'
     )
+      throw Error ('invalid file');
       if (req.file.size > 500000)
         //throw ('invalid file');
         //console.log(body);
@@ -33,5 +34,15 @@ module.exports.uploadProfil = async (req, res) => {
       `${__dirname}/../client/public/uploads/profil/${fileName}`
     )
   );
-  res.status(200).json({ fileName });
+  // Générer automatiquement le chemin de la picture, stocker dans mongoDB: pour l'affichage dans react
+  try {
+    await UserModel.findByIdAndUpdate(
+      req.body.userId,
+      { $set : { picture: "./uploads/profil/" + fileName }},
+      { new: true, upsert: true, setDefaultsOnInsert: true },
+    ).then((docs) =>  res.status(200).send(docs))
+    .catch((err) => res.status(500).send({ message: err}))
+  } catch (err){
+    return res.status(500).send({ message: err})
+  }
 };
